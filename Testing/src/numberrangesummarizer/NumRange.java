0a2c87eaafcf4f9bb34c7efdb814c5494d01e0ac
Thato Lechuti <mokgabolechuti@gmail.com>
2022-05-20 22:09:45 +0200

    Take Home Test from Impact

---
+++ Testing/src/numberrangesummarizer/NumRange.java
@@ -0,0 +1,15 @@
+package numberrangesummarizer;
+
+import java.util.Collection;
+
+public interface NumRange {
+	//collect the input
+    Collection<Integer> collect(String input);
+
+    //get the summarized string
+
+
+    //get the summarized string
+    String summarizeCollection(Collection<Integer> input);
+
+}
