abb8a2bd728db75cd16bcf2fabacbd788c900c8f
Thato Lechuti <mokgabolechuti@gmail.com>
2022-05-20 22:40:13 +0200

    # WARNING: head commit changed in the meantime
    Take Home Test from Impact

--- Testing/src/numberrangesummarizer/NumSummarizer.java
+++ Testing/src/numberrangesummarizer/NumSummarizer.java
@@ -1,4 +1,23 @@
 package numberrangesummarizer;
+/**
+ * @author Werner
+ *
+ * Implement this Interface to produce a comma delimited list of numbers,
+ * grouping the numbers into a range when they are sequential.
+ *
+ *
+ * Sample Input: "1,3,6,7,8,12,13,14,15,21,22,23,24,31
+ * Result: "1, 3, 6-8, 12-15, 21-24, 31"
+ *
+ * The code will be evaluated on
+ *   - functionality
+ *   - style
+ *   - robustness
+ *   - best practices
+ *   - unit tests
+ */
+
+
 
 import java.util.Collection;
 import java.util.*;
